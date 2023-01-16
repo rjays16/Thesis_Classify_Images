43f5329ec1069842280d264c6599b0592f974ed3
Allan Condiman <rjaycondiman81@gmail.com>
2023-01-17 02:35:15 +0800

    Push updates for Counter Class A, Class B, And Rejected Banana Process

--- test_db.m
+++ test_db.m
@@ -3,5 +3,6 @@ curs = exec(conn,'SELECT * FROM user');
 curs = fetch(curs);
 curs.Data
 
+%% mathworks.com/products/database/driver-installation.html
 %% Update user set name='Sample' where id=1
-update(conn,'user',{'name'},{'Buang'},{'WHERE id=1'});
+update(conn,'user',{'name'},{'Sample'},{'WHERE id=1'});
