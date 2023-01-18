d9dcab0832b97172e382da2a764cbff0502493e2
Allan Condiman <rjaycondiman81@gmail.com>
2023-01-19 04:43:24 +0800

    Update for numblobs detect spots on banana

--- test_remove_backgound.m
+++ test_remove_backgound.m
@@ -6,50 +6,34 @@ while true
     picture = imresize(picture,[227,227]);
         image(picture);
         drawnow;
-    
-   Agray = colouredToGray_mex(picture);
-   imageSize = size(picture);
-   numRows = imageSize(1);
-   numCols = imageSize(2);     
         
-    wavelengthMin = 4/sqrt(2);
-    wavelengthMax = hypot(numRows,numCols);
-    n = floor(log2(wavelengthMax/wavelengthMin));
-    wavelength = 2.^(0:(n-2)) * wavelengthMin;
-    
-    deltaTheta = 45;
-    orientation = 0:deltaTheta:(180-deltaTheta);
-    
-    
-    g = gabor(wavelength,orientation);
-    gabormag = imgaborfilt(Agray,g);
-    
-    parfor i = 1:length(g)
-    sigma = 0.5*g(i).Wavelength;
-    K = 3;
-    gabormag(:,:,i) = imgaussfilt(gabormag(:,:,i),K*sigma); 
-    end
-    
-    X = 1:numCols;
-    Y = 1:numRows;
-    [X,Y] = meshgrid(X,Y);
-    featureSet = cat(3,gabormag,X);
-    featureSet = cat(3,featureSet,Y);
-    
-     numPoints = numRows*numCols;
-    X = reshape(featureSet,numRows*numCols,[]);
-    X = bsxfun(@minus, X, mean(X));
-    X = bsxfun(@rdivide,X,std(X));
-    coeff = pca(X);
-    feature2DImage = reshape(X*coeff(:,1),numRows,numCols);
-    L = kmeans(X,2,'Replicates',5);
-	L = reshape(L,[numRows numCols]);
-    
-    Aseg1 = zeros(size(img),'like',img);
-    BW = L == 2;
-    BW = repmat(BW,[1 1 3]);
-    Aseg1(BW) = img(BW);
-    
-    imshow(Aseg1(BW));
-    drawnow limitrate;
+        a = blacked_background(picture);
+         imshow(a);
+         drawnow limitrate;
+         
+         grayImage = a;
+[rows, columns, numberOfColorChannels] = size(grayImage);
+
+if numberOfColorChannels > 1
+  grayImage = rgb2gray(grayImage);
+end
+
+
+
+binaryImage = grayImage == 0;
+binaryImage = imclearborder(binaryImage);
+[labeledImage, numBlobs] = bwlabel(binaryImage);
+coloredLabels = label2rgb (labeledImage, 'hsv', 'k', 'shuffle');
+props = regionprops(labeledImage, 'BoundingBox', 'Centroid');
+
+imshow(picture);
+hold on;
+
+for k = 1 : numBlobs
+   bb = props(k).BoundingBox;
+   bc = props(k).Centroid;
+   rectangle('Position',bb,'EdgeColor','c','LineWidth',2);
+end
+
+drawnow limitrate;
 end
